2cc77a27e418d4f9b6e94445351c600de5697fd0
David Hedbor <david@hedbor.org>
2000-09-20 20:30:50 +0000

    Added copyright notice to source files

--- src/PCRE/pcre.c
+++ src/PCRE/pcre.c
@@ -1,3 +1,25 @@
+/*
+ * Caudium - An extensible World Wide Web server
+ * Copyright © 2000 The Caudium Group
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* PCRE Support - This module adds PCRE support to Pike. */
+
 #include "global.h"
 RCSID("$Id$");
 
@@ -19,7 +41,7 @@ RCSID("$Id$");
 
 
 /* Create a new PCRE regular expression and compile
- * the passed pattern.
+ * and optionally study (optimize) the expression.
  */
 
 void f_pcre_create(INT32 args)
@@ -176,7 +198,7 @@ void f_pcre_match(INT32 args)
     break; 
   }
 }
-
+/* Split the string according to the regexp */
 void f_pcre_split(INT32 args) 
 {
   struct array *arr;	    /* Result array */ 
