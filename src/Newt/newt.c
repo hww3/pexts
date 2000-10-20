be22fb9eb9908e6067ba44921e29d0de8a0e6dce
Marek Habersack <grendello@gmail.com>
2000-10-20 15:27:51 +0000

    Just added the standard pexts disclaimer at the top

--- src/Newt/newt.c
+++ src/Newt/newt.c
@@ -1,5 +1,29 @@
-#include "stralloc.h"
+/*
+ * Pike Extension Modules - A collection of modules for the Pike Language
+ * Copyright © 2000 The Caudium Group
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* Newt Support - This module adds Newt support to Pike. */
+
 #include "global.h"
+RCSID("$Id$");
+
+#include "stralloc.h"
 #include "pike_types.h"
 #include "pike_macros.h"
 #include "object.h"
