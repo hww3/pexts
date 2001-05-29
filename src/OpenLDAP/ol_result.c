c2d11bf46855040e8bbd65583710fe55f8a32b66
Marek Habersack <grendello@gmail.com>
2001-05-29 14:41:11 +0000

    boo

--- src/OpenLDAP/ol_result.c
+++ src/OpenLDAP/ol_result.c
@@ -27,13 +27,30 @@ RCSID("$Id$");
 #include "ol_common.h"
 
 #ifdef HAVE_LIBLDAP
-static struct program  *result_program;
+struct program  *result_program;
 
 #define THIS ((OLSRTORAGE*)get_storage(fp->current_object, result_program))
+#define THIS_PARENT ((OLSTORAGE*)get_storage(Pike_fp->next->current_object, ldap_program))
 
 static void
 f_create(INT32 args)
-{}
+{
+    THIS->conn = THIS_PARENT->conn;
+    THIS->msg = (LDAPMessage*)THIS_PARENT->data;
+
+    pop_n_elems(args);
+}
+
+static void
+f_num_entries(INT32 args)
+{
+    int  ret;
+    
+    pop_n_elems(args);
+    ret = ldap_count_entries(THIS->conn, THIS->msg);
+
+    push_int(ret);
+}
 
 static void
 init_result(struct object *o)
@@ -49,7 +66,7 @@ exit_result(struct object *o)
         free(THIS->msg);
 }
 
-struct program*
+void
 _ol_result_program_init(void)
 {
     start_new_program();
@@ -57,10 +74,13 @@ _ol_result_program_init(void)
 
     set_init_callback(init_result);
     set_exit_callback(exit_result);
+
+    ADD_FUNCTION("create", f_create,
+                 tFunc(tVoid, tVoid), 0);
+    ADD_FUNCTION("num_entries", f_num_entries,
+                 tFunc(tVoid, tInt), 0);
     
     result_program = end_program();
     add_program_constant("Result", result_program, 0);
-    
-    return result_program;
 }
 #endif
