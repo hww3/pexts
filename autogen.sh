b83da61881f63e6ac7da4a305de144c3a14a14e7
Marek Habersack <grendello@gmail.com>
2002-05-14 20:09:06 +0000

    ok, that's a big one but we're 4 months into 2002 :-)

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 cd `dirname $0`
 mydir=`pwd`
+
+aclocal -I aclocal
 for a in `find src tools -name "autogen.sh" |sort`; do
   dir=`dirname $a`
   echo "Running autogen in '$dir'"
   cd $dir >/dev/null 2>&1
   ./autogen.sh $mydir
   cd $mydir >/dev/null 2>&1
-done  
+done
 
 autoheader >/dev/null 2>&1
 autoconf >/dev/null 2>&1
