ade2cfa6eac1fd721de5eb6c74f2cdcbe9af2e05
Marek Habersack <grendello@gmail.com>
2002-12-26 02:17:46 +0000

    licensing stuff - relicensed all modules I wrote under LGPL

--- src/AdminTools/at_quota.c
+++ src/AdminTools/at_quota.c
@@ -1,20 +1,32 @@
 /*
  * Pike Extension Modules - A collection of modules for the Pike Language
- * Copyright © 2000, 2001, 2002 The Caudium Group
+ * Copyright © 2000-2002 The Caudium Group
+ */
+
+/* Copyright (C) 2000-2002 The Caudium Group
+ * Copyright (C) 2000-2002 Marek Habersack
  * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * This file is part of the Pike Extensions package.
+ *
+ * The AdminTools Pexts Module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The AdminTools Pexts Module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the Pike Extensions; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA.
+ *
+ * $Id$
+ */
+
+/*
  *
  * Simple glue for more advanced Unix functions.
  * quotactl support
--- src/_FDF/fdf_global.h
+++ src/_FDF/fdf_global.h
@@ -1,3 +1,30 @@
+/*
+ * Pike Extension Modules - A collection of modules for the Pike Language
+ * Copyright © 2000-2002 The Caudium Group
+ */
+
+/* Copyright (C) 2002 The Caudium Group
+ * Copyright (C) 2002 Marek Habersack
+ * 
+ * This file is part of the Pike Extensions package.
+ *
+ * The FDF Pexts Module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The FDF Pexts Module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the Pike Extensions; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA.
+ *
+ * $Id$
+ */
 #ifndef __FDF_GLOBAL_H
 #define __FDF_GLOBAL_H
 
